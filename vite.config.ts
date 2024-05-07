9e84cea0fdfbbb55fcceca27047545c58ed7e66a
MGPhillips <matthew.phillips.12@ucl.ac.uk>
2024-05-07 15:21:03 +0100

    remove unnecessary vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
-import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
-  plugins: [react(), crx({ manifest }), wasm()],
+  plugins: [react(), crx({ manifest })],
   build: {
     sourcemap: true,
     rollupOptions: {
