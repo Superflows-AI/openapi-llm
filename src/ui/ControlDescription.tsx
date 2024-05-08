390f01056a6a7a3065d6aadeec0a00352de89a28
MGPhillips <matthew.phillips.12@ucl.ac.uk>
2024-05-08 19:32:49 +0100

    Fix text linting error

--- src/ui/ControlDescription.tsx
+++ src/ui/ControlDescription.tsx
@@ -106,7 +106,7 @@ const ControlDescription = () => {
 
           {context.endpoints.length !== 0 && (
               <Text mt={2} color="gray.700" marginBottom="1em">
-                 Click hostname to select endpoints, then click 'Describe Endpoints'
+                 Click hostname to select endpoints, then click `Describe Endpoints`
               </Text>
             )}
           {context.endpoints.length === 0 && (
