7373c560ad9565481734f8303910eafff4664f97
MGPhillips <matthew.phillips.12@ucl.ac.uk>
2024-05-14 11:01:37 +0100

    Add updated dist folder and panel title

---
+++ dist/assets/index.html-717e719a.js
@@ -0,0 +1,8 @@
+import './modulepreload-polyfill-ccc30ec3.js';
+
+chrome.devtools.panels.create(
+    "OpenAPI",
+    "icon16.png",
+    "/src/panel.html",
+);
+//# sourceMappingURL=index.html-717e719a.js.map
