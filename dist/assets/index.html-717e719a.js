a3135badd3ef7ade8c367b0daa8cd91f4f565320
MGPhillips <matthew.phillips.12@ucl.ac.uk>
2024-05-14 13:46:22 +0100

    remove dist folder

--- dist/assets/index.html-717e719a.js
+++
@@ -1,8 +0,0 @@
-import './modulepreload-polyfill-ccc30ec3.js';
-
-chrome.devtools.panels.create(
-    "OpenAPI",
-    "icon16.png",
-    "/src/panel.html",
-);
-//# sourceMappingURL=index.html-717e719a.js.map
